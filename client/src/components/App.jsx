7c98d0aaa5747557a8bc8555b624542151a78f02
raphspies <raphspies@gmail.com>
2021-05-12 17:05:05 -0700

    Default renders to screen

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,3 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
 const App = () => (
   <div>
     <img src="grocery-bags.png" />
@@ -28,3 +31,32 @@ const App = () => (
 );
 
 export default App;
+
+// const App = () => (
+//   <div>
+//     <img src="grocery-bags.png" />
+//     <h1>Grocery List</h1>
+//     <form>
+//       <label> Item
+//         <input name="item" value="" />
+//       </label>
+//       <label> Quantity
+//         <input name="quantity" value="" />
+//       </label>
+//       <button>Add Grocery</button>
+//       <ul className="groceries">
+//         <li>
+//           <span>apples - </span>
+//           <span>4</span>
+//         </li>
+//         <li>
+//           <span>bread - </span>
+//           <span>2</span>
+//         </li>
+//         <li>
+//           <span>etc...</span>
+//         </li>
+//       </ul>
+//     </form>
+//   </div>
+// );
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,7 @@
 // TODO: Render the `App` component to the DOM
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx'
+
+ReactDOM.render(<App />, document.getElementById('root'));
